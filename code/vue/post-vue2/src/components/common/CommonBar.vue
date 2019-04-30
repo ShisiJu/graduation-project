5fa634602e25221c7600ae52c39b0beb12bd2cf0
ShisiJu <jushisi01@163.com>
2019-04-30 15:32:10 +0800

    modify router

--- code/vue/post-vue2/src/components/common/CommonBar.vue
+++ code/vue/post-vue2/src/components/common/CommonBar.vue
@@ -41,31 +41,31 @@
 
 						<el-submenu index="3-1">
 							<template slot="title">
-								学生
+								基本信息
 							</template>
 							<router-link :to="'/admin/students'">
 								<el-menu-item index="3-1-1">学生个人信息</el-menu-item>
 							</router-link>
-							<router-link :to="'/admin/groups'">
-								<el-menu-item index="3-1-2">组信息</el-menu-item>
+							<router-link :to="'/admin/tutors'">
+								<el-menu-item index="3-1-2">导师个人信息</el-menu-item>
 							</router-link>
 						</el-submenu>
 
 						<el-submenu index="3-2">
 							<template slot="title">
-								导师
+								结构信息
 							</template>
-							<router-link :to="'/admin/tutors'">
-								<el-menu-item index="3-2-1">导师个人信息</el-menu-item>
+							<router-link :to="'/admin/institutes'">
+								<el-menu-item index="3-2-1">学院信息</el-menu-item>
 							</router-link>
 							<router-link :to="'/admin/groups'">
-								<el-menu-item index="3-2-2">导师课程信息</el-menu-item>
+								<el-menu-item index="3-2-2">组信息</el-menu-item>
 							</router-link>
 						</el-submenu>
 
 						<el-submenu index="3-3">
 							<template slot="title">
-								课程
+								课程信息
 							</template>
 							<el-menu-item index="3-3-1">课程统计</el-menu-item>
 						</el-submenu>
